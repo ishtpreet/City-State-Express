a5ee7153ba136f71517d646138bad87d3c58c22f
Ishtpreet Singh <500061526@stu.upes.ac.in>
2023-08-21 22:49:36 -0700

    Remove docker | Changed PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
